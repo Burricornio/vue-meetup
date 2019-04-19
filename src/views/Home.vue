9ff61c9f63fb23387d54b5ded44d813384945003
Burricornio <alejandrocf.web@gmail.com>
2019-04-19 17:48:25 +0200

    Added v-carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,53 @@
 <template>
-  <div>
-    <p>The Home page</p>
-  </div>
-
+  <v-container>
+    <v-layout row wrap class="mb-3">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn class="info" large router to="/meetups">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn class="info" large router to="/meetup/new">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="(meetup, i) in meetups" :key="i" :src="meetup.imageURL">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-3">
+      <v-flex class="text-xs-center">
+       <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageURL: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg', id: 'Lemmy', title: 'Meetup in New York'},
+        { imageURL: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Paris_-_Blick_vom_gro%C3%9Fen_Triumphbogen.jpg', id: 'Lemmy', title: 'Meetup in Paris'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+  background-color: rgba(0,0,0,0.5);
+  color: white;
+  font-size: 2em;
+  padding: 10px;
+}
+</style>
+
+
